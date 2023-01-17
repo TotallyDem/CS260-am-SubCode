cfc6e5366c2530dfa2f3a013b47830ba3294c2af
TotallyDem <alexmurray308@gmail.com>
2023-01-16 21:41:17 -0800

    Adding code readability
    Adding docstrings and comments to explain what functions and parts of the code do. Python is pretty readable, but it doesn't hurt to add.

--- assignment1/marblebag.py
+++ assignment1/marblebag.py
@@ -1,8 +1,11 @@
 from random import randint
 marblebag = []
+
 def addmarble():
+    """Prompts the user for what kind of marble to add to the marblebag variable."""
+
     c = -1
-    while ((c > 2) or (c < 0)):
+    while ((c > 2) or (c < 0)): # using a number based selection
         print("0=red\n1=green\n2=blue")
         c = int(input("What color of marble do you want to add? "))
         if ((c > 2) or (c < 0)):
@@ -13,17 +16,23 @@ def addmarble():
         marblebag.append("green")
     elif c == 2:
         marblebag.append("blue")
-    else:
+    else: # Shouldn't be possible, but it doesn't hurt to include.
         print("ERROR: color not in expected range")
+
 def removemarble():
+    """Removes a random marble from the marblebag variable."""
+
     marbleindex = randint(0, len(marblebag)-1)
     print(f"You pulled out a {marblebag[marbleindex]} marble!")
     marblebag.pop(marbleindex)
+
 def marblecount():
+    """Prints out the amount of each kind of marble inside the marblebag variable."""
+
     red = 0
     green = 0
     blue = 0
-    for i in range(0, len(marblebag)):
+    for i in range(0, len(marblebag)): # Iterating through all indexes
         case = marblebag[i]
         if case == "red":
             red += 1
@@ -31,12 +40,14 @@ def marblecount():
             green += 1
         elif case == "blue":
             blue += 1
-        else:
+        else: # Shouldn't be possible unless the user manually edits the variables, but doesn't hurt to incldue.
             print("There is a wierd marble in the bag!")
     print(f"There are {red} red marbles, {green} green marbles, and {blue} blue marbles")
+
+# Just a little function spam to verify the program is working correctly.
 addmarble()
 addmarble()
 addmarble()
 removemarble()
 marblecount()
-print(marblebag)
+print(marblebag) # For a good last measure, just printing the entire list shows that the count is correct.
